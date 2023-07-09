557aca3d1a8e03e5e1f10ba02990454c5a3efd3a
breno.breves <brenocsb@dcc.ufrj.br>
2023-07-09 19:04:53 -0300

    Paginas de login e cadastro funcionais, resta implementar o localStorage

--- src/pages/SignInPage.jsx
+++ src/pages/SignInPage.jsx
@@ -1,21 +1,40 @@
 import styled from "styled-components"
 import { Link, useNavigate } from "react-router-dom"
 import MyWalletLogo from "../components/MyWalletLogo"
+import apiAuth from "../services/apiAuth"
+import { useContext, useState } from "react"
+import { UserContext } from "../contexts/UserContext"
 
 export default function SignInPage() {
   const navigate = useNavigate()
+  const [form , setForm] = useState({email:"", senha:""})
+  const {user , setUser} = useContext(UserContext)
+
+  function handleForm(e){
+    setForm({...form, [e.target.name]: e.target.value})
+  }
 
   function handleLogin(e){
     e.preventDefault()
-    navigate("/home")
+    apiAuth.signIn(form)
+      .then(res => {
+        const {nome , email, saldo, token} = res.data
+        setUser({nome , email, saldo , token})
+        console.log({nome , email, saldo, token})
+        navigate("/home")
+      })
+      .catch(err => {
+        console.log(err.response.data)
+        alert(err.response.data)
+      })
   }
   return (
     <SingInContainer>
       <form onSubmit={handleLogin}>
         <MyWalletLogo />
-        <input placeholder="E-mail" type="email" />
-        <input placeholder="Senha" type="password" autoComplete="new-password" />
-        <button>Entrar</button>
+        <input name="email" value={form.email} onChange={handleForm} placeholder="E-mail" type="email" required/>
+        <input name="senha" value={form.senha} onChange={handleForm} placeholder="Senha" type="password" autoComplete="new-password" required/>
+        <button type="submit">Entrar</button>
       </form>
 
       <Link to="/cadastro">
--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,23 +1,44 @@
 import { Link, useNavigate } from "react-router-dom"
 import styled from "styled-components"
 import MyWalletLogo from "../components/MyWalletLogo"
+import { useState } from "react"
+import apiAuth from "../services/apiAuth"
 
 export default function SignUpPage() {
   const navigate = useNavigate()
+  const [form, setForm] = useState({nome:"" , email:"" , senha:"", confirm:""})
+
+  function handleForm(e){
+    setForm({...form, [e.target.name]: e.target.value})
+  }
 
   function handleRegister(e){
     e.preventDefault()
-    navigate("/") 
+    if(form.senha !== form.confirm){
+      alert("A senha confirmada é diferente da informada")
+      return
+    }
+    const body = form
+    delete body.confirm
+    apiAuth.signUp(body)
+      .then(res => {
+        console.log(res.data)
+        navigate("/") 
+      })
+      .catch(err => {
+        console.log(err.response.data)
+        alert(err.response.data)
+      })
   }
   return (
     <SingUpContainer>
       <form onSubmit={handleRegister}>
         <MyWalletLogo />
-        <input placeholder="Nome" type="text" />
-        <input placeholder="E-mail" type="email" />
-        <input placeholder="Senha" type="password" autoComplete="new-password" />
-        <input placeholder="Confirme a senha" type="password" autoComplete="new-password" />
-        <button>Cadastrar</button>
+        <input name="nome" value={form.nome} onChange={handleForm} placeholder="Nome" type="text" required/>
+        <input name="email" value={form.email} onChange={handleForm} placeholder="E-mail" type="email" required/>
+        <input name="senha" value={form.senha} onChange={handleForm} placeholder="Senha" type="password" autoComplete="new-password" required/>
+        <input name="confirm" value={form.confirm} onChange={handleForm} placeholder="Confirme a senha" type="password" autoComplete="new-password" required/>
+        <button type="submit">Cadastrar</button>
       </form>
 
       <Link to="/">
